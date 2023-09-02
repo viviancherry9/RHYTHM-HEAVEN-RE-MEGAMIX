7ab90bff8689fff3253f8086ef02b522c422a3c1
TheAlternateDoctor <thealternatedoc@gmail.com>
2023-09-02 18:57:10 +0200

    Added Rockers & Baking Pests

--- tickflow/compile.sh
+++ tickflow/compile.sh
@@ -1,9 +1,5 @@
 #/usr/bin/sh
 java -jar ../../tickompiler.jar c ./ Compiled/
 java -jar ../../tickompiler.jar p Compiled/ "base.bin edits/edit.bin"
-if which citra 2>/dev/null; then
-    cp -f C00.bin ~/.config/citra-emu/sdmc/rhmm
-else
-    # assume flatpak
-    cp -f C00.bin ~/.var/app/org.citra_emu.citra/data/citra-emu/sdmc/rhmm
-fi
+mv -f C00.bin ~/.local/share/citra-emu/sdmc/rhmm
+killall -9 citra-qt

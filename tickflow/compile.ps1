c20e76a13b8f98b2c4e9efd8b0a8db44be53824f
TheAlternateDoctor <thealternatedoc@gmail.com>
2023-09-16 21:45:35 +0200

    Fixed up compile.ps1 to be more universally usable

--- tickflow/compile.ps1
+++ tickflow/compile.ps1
@@ -1,3 +1,3 @@
-﻿java8 -jar ..\..\tickompiler.jar c .\ Compiled\
-java8 -jar ..\..\tickompiler.jar p Compiled\ "base.bin edits\edit.bin"
+﻿java -jar ..\..\tickompiler.jar c .\ Compiled\
+java -jar ..\..\tickompiler.jar p Compiled\ "base.bin edits\edit.bin"
 mv -force C00.bin $Env:APPDATA\Citra\sdmc\rhmm

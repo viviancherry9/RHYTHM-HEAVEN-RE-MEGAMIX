3f145dd94b2663814f025dbd1f3731ec610cddc6
TheAlternateDoctor <thealternatedoc@gmail.com>
2023-09-30 00:15:08 +0200

    small comment correction in checkTimg

--- scripts/checkTimg.sh
+++ scripts/checkTimg.sh
@@ -1,4 +1,4 @@
-#First rebuild coffee_common
+#First extract coffee_common
 cd ../USlayout/coffee_common/
 python2 ../../utils/sarc.py -xzf coffee_common.zlib
 
@@ -23,7 +23,7 @@ cd ../map_room/
 python2 ../../utils/sarc.py -xzf map_room.zlib
 
 
-#cleanup
+#Clean the folders we don't need
 cd ../
 rm -rf coffee_common/anim coffee_common/coffee_common_ coffee_common/blyt coffee_common/font
 rm -rf coffee_game/anim coffee_game/coffee_game_ coffee_game/blyt
@@ -32,8 +32,8 @@ rm -rf map_common/anim map_common/map_common_ map_common/blyt map_common/font
 rm -rf map_figure/anim map_figure/map_figure_ map_figure/blyt
 rm -rf map_room/anim map_room/map_room_ map_room/blyt map_room/font
 
-#We wait for user input before re-cleaning
-read -p "Press [Enter] key once you're done checking!"
+#We wait for user input before cleaning the timg folder
+read -p "Press [Enter] once you're done checking!"
 rm -rf coffee_common/timg
 rm -rf coffee_game/timg
 rm -rf coffee_shopItem/timg
